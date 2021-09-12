712fa3b2fa225b8d1acf335bc3dbb8aed3bf6f7e
Horea <horea.constandache@3pillarglobal.com>
2021-09-12 12:33:34 +0300

    added menu

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,14 @@
+import Layout from '../components/layout/Layout'
+import Menu from '../components/menu/Menu'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Menu/>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
